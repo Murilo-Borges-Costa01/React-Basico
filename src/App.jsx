06ae5306a4752a688c4d4493c0b3b648f93659e9
Murilo-Borges-Costa01 <muriloborges481@gmail.com>
2026-05-06 17:17:36 -0300

    Salvo

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,42 @@ export default function App () {
     fontFamilly: 'Helvetica'
   }
 
+
+  const estilo2 = {
+    color: 'green',
+    fontSize: '20px',
+    fontFamilly: 'Helvetica'
+  }
+
+  const estilo3 = {
+    color: 'red',
+    fontSize: '20px',
+    fontFamilly: 'Helvetica'
+  }
+
+// EXERCICIO:
+
+  const luana = {
+  cliente: 'Luana',
+  idade: 27,
+  compras: 2000,
+  ativa: true,
+};
+
+
+const mario = {
+  cliente: 'Mario',
+  idade: 31,
+  compras: 100000,
+  ativa: false,
+};
+
+const App = () => {
+  const dados = luana;
+
+  return <div></div>;
+};
+
   return(
     <>
     <p>Olá, seja bem-vindo {nome}</p>
@@ -47,6 +83,30 @@ export default function App () {
     <h1 style={estilo1}>Murilo Borges Costa 001</h1>
     <h1 style={{color: 'red', fontSize: "20px"}}>Murilo Borges Costa 002</h1>
     <h1 style={{color: false ? 'red' : 'violet'}}>Murilo Borges Costa 003</h1>
+    <p>
+    Murilo {
+    Murilo.possuiFaculdade ?
+    <span style={{color: 'blue'}}>Sim, possui faculdade</span>
+    :
+    <span style={{color: 'red'}}>Não, possui faculdade</span> }
+    </p>
+    <h1 style={{color: Murilo.possuiFaculdade ? "blue" : "red"}}>Murilo</h1>
+
+    {/* // Mostre os dados da aplicação, como aprensetado no vídeo
+// Não utilize CSS externo, use o style para mudar as cores
+// Se a situação estiver ativa pinte de verde, inativa vermelho
+// Se o gasto for maior que 10000 mostre uma mensagem */}
+<h1> {mario.ativa ? <span style={estilo2}>Conta do Mario Ativa</span> : <span style={estilo3}>Conta do Mario Inativa</span>}</h1>
+<h1>{luana.ativa ? <span style={estilo2}>Conta da Luana Ativa</span> : <span style={estilo3}>Conta da Luana Inativa</span>}</h1>
+
+<h1>{luana.compras > 10000 ? <span style={estilo2}>Compras maior que 10000</span> : <span style={estilo3}>Compras menor que 10000</span>}</h1>
+<h1>{mario.compras > 10000 ? <span style={estilo2}>Compras maior que 10000</span> : <span style={estilo3}>Compras menor que 10000</span>}</h1>
+
+
+
+
     </>
   )
 }
+
+

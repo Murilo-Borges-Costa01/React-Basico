8dcb6a6d4ccae8285ecee3084c6fa8d2a187d38e
Murilo-Borges-Costa01 <muriloborges481@gmail.com>
2026-05-11 17:22:58 -0300

    Somar coisas

--- src/App.jsx
+++ src/App.jsx
@@ -127,8 +127,62 @@ const somarTotalCursos = dado.cursos
 //   return <div></div>;
 // };
 
+const empresa = [
+  <li key= "e1">Apple</li>,
+  <li key= "e2">Google</li>
+];
 
 
+const filmes = ["Vingadores", "Thor", "Flesh"];
+
+const livros = [
+  { nome: "Dom Casmurro", ano: 1899 },
+  { nome: "O Pequeno Principe", ano: 1943 },
+  { nome: "Capitaes da Areia", ano: 1937 },
+];
+
+const cores =[
+
+'#CAD338',
+
+'#2472c6','#9a382d' ];
+
+
+
+// Organize os produtos como mostrado na imagem abaixo
+// Mostre apenas produtos que forem mais caros que R$ 1500
+const produtos = [
+{
+id: 1,
+nome: 'Smartphone',
+preco: 'R$ 2000',
+cores: ['#29d8d5', '#252a34', '#fc3766'],
+},
+{
+  id: 2,
+nome: 'Notebook',
+preco: 'R$ 3000',
+cores: ['#ffd045', '#d4394b', '#f37c59'],
+},
+{
+id: 3,
+nome: 'Tablet',
+preco: 'R$ 1500',
+cores: ['#365069', '#47c1c8', '#f95786'],
+},
+];
+
+const valorProd = produtos
+.map(item => +item.preco.replace('R$','').trim())
+.reduce((acumulador, itemAtual) => {
+  return acumulador + itemAtual
+}, 0)
+
+
+
+const App = () => {
+return <section></section>;
+}
 
   return(
     <>
@@ -201,37 +255,61 @@ style={{color: dados.ativa ? 'green' : 'red' } }>
   <h1>Lista de Cursos</h1>
 
 <ul>
-
 {dado.cursos.map (curso =>(
-
 <li key={curso.curso}>{curso.curso}</li>
-
 ))}
-
 </ul>
 </div>
 
+<ul>
+  {empresa}
+</ul>
+
+<ul>
+  {filmes.map(filme => (
+    <li key={filme}>{filme}</li>
+  ))}
+</ul>
+
+<ul>
+  {livros.filter(livro => livro.ano >= 1998)
+  .map(livro => (
+    <li key={livro.nome}>{livro.nome}, {livro.ano}</li>
+  ))}
+</ul>
+
+<ul>
+{cores.map(cor => (
+<li style={{backgroundColor: cor, color: "white"}} key={cor}>{cor}</li>
+))}
+</ul>
+
+<h1>O total de tudo é: {valorProd}</h1>
+{valorProd >  1500 && <h1 style={{color: "red"}}>Você esta gastando muito</h1>}
+
+{
+  produtos.filter(produto)
+}
 
 
+{
+produtos.filter(produto => +produto.preco.replace('R$', '').trim() > 1500)
 
+.map((produto)=>(
+<div key={produto.id}>
+<h1>{produto.nome}</h1>
 
+<p>Preço: {produto.preco}</p>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<ul>
+  {produto.cores.map (cor => (
+    <li key={cor} style={{backgroundColor: cor, color: "white"}}>{cor}</li>
+  ))}
+</ul>
+</div>
+))
+}
     </>
   )
 }
-
 
